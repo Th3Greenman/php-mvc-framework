f1327a6ea4d93cf9e0baead9c40afa2bf8a2d056
Th3Greenman <zervan_hoving@live.nl>
2020-12-04 15:50:46 +0100

    added views

---
+++ views/register.php
@@ -0,0 +1,22 @@
+<?php
+
+use app\core\form\Form;
+
+?>
+<h1>Register</h1>
+
+<?php $form = Form::begin('', 'post'); ?>
+    <div class="row">
+        <div class="col">
+            <?= $form->field($model, 'firstname'); ?>
+        </div>
+        <div class="col">
+            <?= $form->field($model, 'lastname'); ?>
+        </div>
+    </div>
+    <?= $form->field($model, 'email'); ?>
+    <?= $form->field($model, 'password')->passwordField(); ?>
+    <?= $form->field($model, 'confirm_password')->passwordField(); ?>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+<?php Form::end(); ?>
